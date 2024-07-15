7a5ef2e1f0f41501d904696903caaf80ed3c15cd
yusib <yusibovnail@gmail.com>
2024-07-15 19:21:44 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+
+    private static SessionFactory sessionFactory;
+
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
+            configuration.setProperty("hibernate.connection.username", "root");
+            configuration.setProperty("hibernate.connection.password", "PolinaTimur120593!");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+            configuration.setProperty("hibernate.show_sql", "true");
+
+
+            configuration.addAnnotatedClass(User.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
@@ -13,7 +44,7 @@ public class Util {
 
     private static final String PASS = "PolinaTimur120593!";
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
 
         Connection conn = null;
 
@@ -24,6 +55,10 @@ public class Util {
         }
 
         return conn;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 
 }
